7e8cc52739b53b4ff9f9c49b0128fd8c7c833b7c
MatheusMiranda <mpssm@cesar.org.br>
2024-08-27 20:58:41 -0300

    chore: get users

---
+++ main.py
@@ -0,0 +1,47 @@
+from typing import List
+from uuid import uuid4
+from fastapi import FastAPI
+
+from models import Gender, Role, User
+
+app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=uuid4(), 
+        first_name="Matheus", 
+        last_name="Miranda",
+        gender=Gender.male,
+        roles=[Role.user, Role.admin]
+    ),
+    User(
+        id=uuid4(), 
+        first_name="Ana", 
+        last_name="Silva",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(), 
+        first_name="Priscilla", 
+        last_name="Santana",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(), 
+        first_name="Hadja", 
+        last_name="Costa",
+        gender=Gender.female,
+        roles=[Role.student]
+    )
+]
+
+
+@app.get("/")
+async def root():
+    return {"Hello": "World"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
