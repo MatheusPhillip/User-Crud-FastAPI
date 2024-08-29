695788b7c0dbc63e04704e61e449de4d6e40f467
MatheusMiranda <mpssm@cesar.org.br>
2024-08-29 13:43:20 -0300

    feat: add update user endpoint

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 from uuid import UUID, uuid4
 from fastapi import FastAPI, HTTPException
 
-from models import Gender, Role, User
+from models import Gender, Role, User, UserUpdateRequest
 
 app = FastAPI()
 
@@ -62,3 +62,22 @@ async def delete_user(user_id: UUID):
         status_code=404,
         detail=f"user with id: {user_id} does not exist"
     )
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return "user updated"
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist"
+    )
+        
